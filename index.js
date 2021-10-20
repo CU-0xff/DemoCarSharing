74cd2063ca9d6cd97cad2a932a9b8776731e7ed1
CU-0xff <frank@cu-0xff.de>
2021-10-20 22:21:34 +0200

    Checked for the first issues but there is one with the a SQL injection that needs larger attention

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const config = require('./config/VTSconfig.js');
-const vehicleDb = require('./dbInterface/vehicleDB_test.js');
-//const vehicleDb = require('./dbInterface/vehicleDb.js');
+//const vehicleDb = require('./dbInterface/vehicleDB_test.js');
+const vehicleDb = require('./dbInterface/vehicleDb.js');
 
 const app = express();
 
+app.get(config.urlPrefix+`vehicles/list`, (req, res) =>{
+    let vehicleList = vehicleDb.getVehicleList();
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    return res.send(vehicleList);
+});
+
+app.get(config.urlPrefix+`vehicles/:vehicleId`, (req,res) => {
+    let vehicleDetail = vehicleDb.getVehicleDetails(req.params.vehicleId);
+    return res.send(vehicleDetail);
+});
 
 app.listen(config.PORT, () => 
     console.log(`Vehicle Telemetry Service is listening on port ${config.PORT}!`),
