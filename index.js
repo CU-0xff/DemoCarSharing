cc6cddd56962a3928bfa136fb63d2d92fb86bb68
CU-0xff <frank@cu-0xff.de>
2022-06-28 17:51:59 +0200

    CHanges

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const config = require('./config/VTSconfig.js');
-const vehicleDb = require('./dbInterface/vehicleDB_test.js');
-//const vehicleDb = require('./dbInterface/vehicleDb.js');
+//const vehicleDb = require('./dbInterface/vehicleDB_test.js');
+const vehicleDb = require('./dbInterface/vehicleDb.js');
 
 const app = express();
 
@@ -9,3 +9,13 @@ const app = express();
 app.listen(config.PORT, () => 
     console.log(`Vehicle Telemetry Service is listening on port ${config.PORT}!`),
 );
+
+app.get(config.urlPrefix+`vehicles/list`, (req, res) =>{
+    let vehicleList = vehicleDb.getVehicleList();
+    return res.send(vehicleList);
+})
+
+app.get(config.urlPrefix+`vehicles/:vehicleId`, (req,res) => {
+    let vehicleDetail = vehicleDb.getVehicleDetails(req.params.vehicleId);
+    return res.send(vehicleDetail);
+})
