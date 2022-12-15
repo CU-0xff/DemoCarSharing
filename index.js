a768381f09949aa7f29474a634d25546bb4b3e78
Frank Fischer <Frank@Fischer.mt>
2022-12-15 12:23:53 +0100

    Faulty

--- index.js
+++ index.js
@@ -9,3 +9,32 @@ const app = express();
 app.listen(config.PORT, () => 
     console.log(`Vehicle Telemetry Service is listening on port ${config.PORT}!`),
 );
+
+app.get(config.urlPrefix+`vehicles/list`, (req, res) =>{
+    let vehicleList = vehicleDb.getVehicleList();
+    return res.send(vehicleList);
+})
+
+app.get(config.urlPrefix+`vehicles/:vehicleId`, (req,res) => {
+    let vehicleDetail = vehicleDb.getVehicleDetails(req.params.vehicleId);
+    return res.send(vehicleDetail);
+})
+
+app.put(config.urlPrefix+'vehicles/:vehicleId', (req, res) => {
+    reqAction = req.params.reqAction;
+    returnValue = "";
+    switch(toUpper(reqAction)) {
+        case "RESERVE":
+            returnValue = vehicelDb.reserveVehicle(req.params.vehicleId);
+            break;
+        case "BOOK":
+            returnValue = vehicelDb.reserveVehicle(req.params.vehicleId);
+            break;
+        default:
+            returnValue = "ERROR: Unknown verb";
+            break;
+    }
+
+    return returnValue;
+
+})
