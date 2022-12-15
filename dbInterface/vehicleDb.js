bb5ab9bca754f1477867e46893057cb905954d42
Frank Fischer <Frank@Fischer.mt>
2022-12-15 12:10:12 +0100

    Faulty commit

--- dbInterface/vehicleDb.js
+++ dbInterface/vehicleDb.js
@@ -28,6 +28,10 @@ function getVehicleDetails(license) {
     })
 }
 
+function reserveVehicle(license) {
+    return `SUCCESS $licence`;
+}
+
 module.exports = {
     getVehicleList : getVehicleList,
     getVehicleDetails : getVehicleDetails,
