2f33cfda1faaf67f79534ad4d5a4e3e931be224e
Frank Fischer <Frank@Fischer.mt>
2023-03-20 11:24:20 +0100

    Bla

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const config = require('./config/VTSconfig.js');
-const vehicleDb = require('./dbInterface/vehicleDB_test.js');
-//const vehicleDb = require('./dbInterface/vehicleDb.js');
+//const vehicleDb = require('./dbInterface/vehicleDB_test.js');
+const vehicleDb = require('./dbInterface/vehicleDb.js');
 
 const app = express();
+
+app.get(config.urlPrefix+`vehicles/list`, (req, res) =>{
+    let vehicleList = vehicleDb.getVehicleList();
+    return res.send(vehicleList);
+})
+
+app.get(config.urlPrefix+`vehicles/:vehicleId`, (req,res) => {
+    let vehicleDetail = vehicleDb.getVehicleDetails(req.params.vehicleId);
+    return res.send(vehicleDetail);
+})
+
+app.put(config.urlPrefix+'vehicles/:vehicleId', (req, res) => {
+    reqAction = req.params.reqAction;
+    returnValue = "";
+    switch(toUpper(reqAction)) {
+        case "RESERVE":
+            returnValue = vehicelDb.reserveVehicle(req.params.vehicleId);
+            break;
+        case "BOOK":
+            returnValue = vehicelDb.reserveVehicle(req.params.vehicleId);
+            break;
+        default:
+            returnValue = "ERROR: Unknown verb";
+            break;
+    }
+
+    return returnValue;
+
+})
 
 
 app.listen(config.PORT, () => 
