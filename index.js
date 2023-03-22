52a05ac694916dc108b0ffc350652839c715c913
Frank Fischer <Frank@Fischer.mt>
2023-03-22 16:31:55 +0100

    Demo Bla

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const config = require('./config/VTSconfig.js');
-const vehicleDb = require('./dbInterface/vehicleDB_test.js');
-//const vehicleDb = require('./dbInterface/vehicleDb.js');
+//const vehicleDb = require('./dbInterface/vehicleDB_test.js');
+const vehicleDb = require('./dbInterface/vehicleDb.js');
 
 const app = express();
 
@@ -9,3 +9,37 @@ const app = express();
 app.listen(config.PORT, () => 
     console.log(`Vehicle Telemetry Service is listening on port ${config.PORT}!`),
 );
+
+app.get(config.urlPrefix+`vehicles/list`, (req, res) =>{
+    let vehicleList = vehicleDb.getVehicleList();
+    return res.send(vehicleList);
+})
+
+app.get(config.urlPrefix+`vehicles/:vehicleId`, (req,res) => {
+    let vehicleDetail = vehicleDb.getVehicleDetails(req.params.vehicleId);
+    return res.send(vehicleDetail);
+})
+
+app.put(config.urlPrefix+'vehicles/:vehicleId', (req, res) => {
+    reqAction = req.params.reqAction;
+    returnValue = "";
+    switch(toUpper(reqAction)) {
+        case "RESERVE":
+            returnValue = vehicelDb.reserveVehicle(req.params.vehicleId);
+            break;
+        case "BOOK":
+            returnValue = vehicelDb.reserveVehicle(req.params.vehicleId);
+            break;
+        default:
+            returnValue = "ERROR: Unknown verb";
+            break;
+    }
+
+    return returnValue;
+
+})
+
+vehiceleTestDb:
+function reserveVehicle(license) {
+    return `SUCCESS $licence`;
+}
