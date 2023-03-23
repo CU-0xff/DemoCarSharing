a8757859f04a5470b26bdd2d8e4374b43798c76d
cha3ban <chaaban.barakat@snyk.io>
2023-03-23 12:13:17 +0000

    test

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const vehicleDb = require('./dbInterface/vehicleDB_test.js');
 
 const app = express();
 
+app.get(config.urlPrefix+`vehicles/list`, (req, res) =>{
+    let vehicleList = vehicleDb.getVehicleList();
+    return res.send(vehicleList);
+})
+
+app.get(config.urlPrefix+`vehicles/:vehicleId`, (req,res) => {
+    let vehicleDetail = vehicleDb.getVehicleDetails(req.params.vehicleId);
+    return res.send(vehicleDetail);
+})
 
 app.listen(config.PORT, () => 
     console.log(`Vehicle Telemetry Service is listening on port ${config.PORT}!`),
